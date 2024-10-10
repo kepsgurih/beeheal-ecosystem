6715ea08a0c090207cc69f241627fbc4504e9d44
kepsgurih <kepsgurih@pm.me>
2024-10-10 22:35:49 +0700

    :wrench: adding

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,9 +3,6 @@
 import axios, { AxiosError } from 'axios'
 import { cookies } from "next/headers";
 
-const sessionPassword = process.env.SESSION_PASSWORD as string;
-if (!sessionPassword) throw new Error("SESSION_PASSWORD is not set");
-
 export const registerServices = async ({ name, email, password }: { name: string, email: string, password: string }) => {
     try {
         const resp = await axios({
@@ -34,6 +31,7 @@ export const registerServices = async ({ name, email, password }: { name: string
 }
 
 export const loginServices = async (_currentState: unknown, formData: FormData) => {
+
     try {
         const resp = await axios({
             url: `${process.env.NEXT_INTIAL_REQ as string + process.env.NEXT_PUBLIC_VERCEL_URL as string}/api/v1/auth/login`,
@@ -44,11 +42,12 @@ export const loginServices = async (_currentState: unknown, formData: FormData) 
             }
         })
         const token = resp.data.token;
-        console.info('INFO BERHASIL', token, process.env.NODE_ENV)
         cookies().set('auth_token', token, {
             sameSite: 'lax',
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: true,
+            path: '/',
+            maxAge: 2592000
         });
     }
 
