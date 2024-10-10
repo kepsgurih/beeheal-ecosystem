919a61b3aa9d5556b601ba20fdbea06194532485
kepsgurih <kepsgurih@pm.me>
2024-10-10 21:55:10 +0700

    :wrench: adding lax

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -34,16 +34,9 @@ export const registerServices = async ({ name, email, password }: { name: string
 }
 
 export const loginServices = async (_currentState: unknown, formData: FormData) => {
-    console.log(`${process.env.NEXT_INTIAL_REQ as string + process.env.NEXT_PUBLIC_VERCEL_URL as string}/api/v1/auth/login`)
-    console.info('INFO1', process.env.AAA1)
-    console.info('INFO2', process.env.AAA2)
-    console.info('INFO3', process.env.AAA3)
-    console.info('INFO4', process.env.AAA4)
-    console.info('INFO5', process.env.AAA5)
-    console.info('INFO6', process.env.AAA6)
     try {
         const resp = await axios({
-            url: `${'https://' + process.env.NEXT_PUBLIC_VERCEL_URL || process.env.PUBLIC_URL}/api/v1/auth/login`,
+            url: `${process.env.NEXT_INTIAL_REQ as string + process.env.NEXT_PUBLIC_VERCEL_URL as string}/api/v1/auth/login`,
             method: 'POST',
             data: {
                 email: formData.get('email'),
@@ -51,8 +44,9 @@ export const loginServices = async (_currentState: unknown, formData: FormData) 
             }
         })
         const token = resp.data.token;
+        console.info('INFO BERHASIL', token, process.env.NODE_ENV)
         cookies().set('auth_token', token, {
-            sameSite: 'strict',
+            sameSite: 'lax',
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
         });
