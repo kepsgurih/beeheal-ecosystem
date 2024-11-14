69114f8265651e7382ddbdad2959bc45fe1d554e
kepsgurih <kepsgurih@pm.me>
2024-11-14 18:33:39 +0700

    :wrench: fix put api

--- src/app/api/v1/tasks/[id]/route.ts
+++ src/app/api/v1/tasks/[id]/route.ts
@@ -19,8 +19,8 @@ export async function GET(request: Request, { params }: { params: Promise<{ id: 
     }
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-    const { id } = params;
+export async function PUT(request: Request, { params }: { params: Promise<{ id: string }> }) {
+    const id = (await params).id;
     const updates = await request.json();
 
     if (!id) return NextResponse.json({ error: "ID is required" }, { status: 400 });
